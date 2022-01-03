3e1ac497db466fe9ce74575b733c2e958e8a9aa5
pablolauro <pablo.lauro@gmail.com>
2022-01-03 10:32:55 -0300

    Subindo projeot

---
+++ ReservaMesasWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReservaMesasWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
